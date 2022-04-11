41c63fd24fa4d0290a1f5e232eb403416bf68f2f
William Rodrigues <William.Rodrigues@flex.com>
2022-04-11 11:27:07 -0300

    Otimização do código

--- exercicio-contador-de-notas-alunos.py
+++ exercicio-contador-de-notas-alunos.py
@@ -10,44 +10,28 @@ ao digitar cada uma das notas deve ser exibida uma mensagem no seguinte padrão:
 VOCÊ ESTÁ DIGITANDO AS NOTAS DOS ALUNOS PARES (ou ímpares, quando for o caso).
 POR FAVOR, INSIRA A NOTA DO ALUNO DE NÚMERO x."""
 
-
-#TotalAlunos = int(50)
-AlunosImpares = []
-AlunosPares = []
 TotalImpar = []
 TotalPar = []
 
-for TotalAlunos in range(1,50):
-    if TotalAlunos % 2 != 0:
-        AlunosImpares.append(TotalAlunos)
-    else:
-        AlunosPares.append(TotalAlunos)
-
-print("VOCÊ ESTÁ DIGITANDO A NOTA DOS ALUNOS DE NÚMEROS ÍMPARES:")
+print("VOCÊ ESTÁ DIGITANDO A NOTA DOS ALUNOS DE NÚMEROS ÍMPARES:VOCÊ ESTÁ DIGITANDO A NOTA DOS ALUNOS DE NÚMEROS ÍMPARES:")
 for AlunosImpares in range(1,50,2):
     NotasImpar = float(input(f"Por favor insira a nota do aluno de número {AlunosImpares}:"))
-    TotalImpar.append(NotasImpar)
+    TotalImpar.append(NotasImpar) 
 
 print("VOCÊ ESTÁ DIGITANDO A NOTA DOS ALUNOS DE NÚMEROS PARES:")
 for AlunosPares in range(2,51,2):
     NotasPar = float(input(f"Por favor insira a nota do aluno de número {AlunosPares}:"))
     TotalPar.append(NotasPar)
 
-SomaImpar = sum(TotalImpar)
-SomaPar = sum(TotalPar)
+MediaTurmaImpar = sum(TotalImpar) / len(TotalImpar)
+MediaTurmaPar = sum(TotalPar) / len(TotalImpar)
 
-MediaTurmaImpar = SomaImpar / 25
-MediaTurmaPar = SomaPar / 25
+print(f"Média da turma Ímpar: {MediaTurmaImpar}")
+print(f"Média da turma Par: {MediaTurmaPar}")
 
 if MediaTurmaImpar > MediaTurmaPar:
-    print(f"Média da turma Ímpar: {MediaTurmaImpar}")
-    print(f"Média da turma Par: {MediaTurmaPar}")
     print(f"A Média dos alunos Ímpar foi maior que da turma par: {MediaTurmaImpar}")
 elif MediaTurmaPar > MediaTurmaImpar:
-    print(f"Média da turma Par: {MediaTurmaPar}")
-    print(f"Média da turma Ímpar: {MediaTurmaImpar}")
     print(f"A Média dos alunos Par foi maior que da turma ímpar: {MediaTurmaPar}")
-elif MediaTurmaImpar == MediaTurmaPar:
-    print(f"Média da turma Par: {MediaTurmaPar}")
-    print(f"Média da turma Ímpar: {MediaTurmaImpar}")
+else: #MediaTurmaPar == MediaTurmaImpar:
     print(f"Houve um empate entre a média das turmas Par e Ímpar")
